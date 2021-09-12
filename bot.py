6ae7c9b7c5670e769893ad3a22a797fbf11e6f61
EIRBLAST <64648494+EIRBLAST@users.noreply.github.com>
2021-09-12 10:39:43 +0200

    🔄 client fix
    again help

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN     = os.environ['TOKEN']
 #bot client
 client = RaspailAssistant()
 #slash command client
-slash = SlashCommand(client)
+slash = SlashCommand(client,sync_command = True, sync_on_cog_reload = True,)
 
 #main extensions
 main_extension = [
@@ -17,17 +17,21 @@ main_extension = [
 
 guild_ids = [879451596247933039] # Put your server ID in this array.
 
+for file in main_extension:
+    client.load_extension(file)
+    print(f'loaded {file}')
+
 @slash.slash(name="ping", guild_ids=guild_ids)
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
+
 #called when the bot starts
 @client.event
 async def on_ready():
     #load the bot cogs
-    for file in main_extension:
-            client.load_extension(file)
-            print(f'loaded {file}')
     print('Loaded extensions')
+
+
 
 if __name__ == '__main__':
     
