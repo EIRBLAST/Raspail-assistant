4757924dab3d2ae340ec221caeaf8a38d62953db
JulesMichael <julesmichaelmail@gmail.com>
2021-11-14 21:23:54 +0100

    Lightens the file

--- cogs/planning_commands.py
+++ cogs/planning_commands.py
@@ -1,203 +1,19 @@
-from discord.ext import commands
-from discord_slash import SlashCommand , cog_ext, SlashContext ,ComponentContext
-from discord_slash.utils.manage_components import create_select,create_button, create_select_option, create_actionrow,wait_for_component
-from discord_slash.model import ButtonStyle
-from class_file import *
-from PIL import Image, ImageDraw,ImageFont
-
-import json
 import io
-import discord
-
-from datetime import datetime, timedelta, date
 from time import time
+import datetime
 
+from discord.ext import commands
+from discord_slash import ComponentContext, SlashCommand, SlashContext, cog_ext
+from discord_slash.model import ButtonStyle
+from discord_slash.utils.manage_components import (create_actionrow,
+                                                   create_button,
+                                                   create_select,
+                                                   create_select_option,
+                                                   wait_for_component)
 
-datas = json.load(open("datas/colloscope.json", "r"))
-
-DAYS = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"]
-
-def get_events_of_the_day(day:datetime.date,grp:int) -> list:
-    """Return a list of events of the day `day`od group `grp`
-
-    Args:
-        day (datetime.date): The day
-        grp (int): The group
-    
-    Returns:
-        [list]: A list of events  
-    """    
-    monday = day - timedelta(days = day.weekday())
-    column_index = datas["mondays"].index(monday.strftime("%d/%m/%Y")) 
-
-    events = []
-    for line in datas["planning"]:
-        if line["grps"][column_index] == int(grp) and line["timedelta"]["days"] == day.weekday():
-            events.append(line)
-
-    events.sort(key= lambda line : timedelta(**line["timedelta"]))
-    
-    return events
-
-def get_week_parity(day):
-    monday = day - timedelta(days = day.weekday())
-    column_index = datas["mondays"].index(monday.strftime("%d/%m/%Y"))
-
-    return datas["weeknumber"][column_index] % 2
-
-def informatique_parity(day):
-    monday = day - timedelta(days = day.weekday())
-    column_index = datas["mondays"].index(monday.strftime("%d/%m/%Y"))
-    res = datas["informatique"][column_index]
-    return 'pair'*(res == 'B') + 'impair'*(res == 'A') + 'entier'*(res == 'C')
-
-
-def IsParite(parite:str,group_number:int,week_parite:int) ->bool:
-    """this function compute if the group attend the course
-
-    Args:
-        parite (str): the parite string
-        group_number (int): the group id
-        week_parite (int): the week parity
-
-    Returns:
-        bool: wether the group attend or not
-    """
-    if parite == 'entier' : return True
-    if parite == 'pair':
-        return group_number%2 != week_parite
-    elif parite == 'impair':
-        return group_number%2 == week_parite
-    else:
-        return True
-
-def get_day_planing(groupe:int,week_parity:int,jour:dict,monday:datetime) -> Image.Image:
-    #declare constant
-    X_LENGHT = 300
-    Y_LENGHT = 100
-
-    vertical_alignement = lambda txt, font: X_LENGHT // 2 - font.getsize(txt)[0] // 2
-
-    # load fonts and create the image
-    im =  Image.new('RGB', (X_LENGHT, Y_LENGHT*11), color = 'white')
-    fnt = ImageFont.truetype('datas/Roboto-Regular.ttf', Y_LENGHT//5)
-    fnt_bold = ImageFont.truetype('datas/Roboto-Bold.ttf', Y_LENGHT//4)
-    
-    jour_index = jour['jour']
-
-    # cours
-    liste_cours = [cours for cours in jour['cours'] if IsParite(cours['parite'],groupe,week_parity) and cours['nom'] != 'Informatique']
-    if IsParite(informatique_parity(monday),groupe,week_parity) and "Informatique" in (cours['nom'] for cours in jour['cours']):
-        if informatique_parity(monday) == 'entier': salle = 'B411'
-        else : salle = 'B401'
-        liste_cours.append({"nom":"Informatique","salle":salle,"heures":[10,11],"parite":"INFO"})
-    liste_image = []
-
-    for i,cours in enumerate(liste_cours):
-        # block size
-        size = len(cours['heures'])
-        
-        # bloc color
-        colors = {"Physique": "purple" ,"Maths": "green" ,"Anglais": "blue","S2I": "yellow","Français": "pink","Informatique": "cyan","DS": "red","colle":"grey","TIPE":"yellow", "tp": "yellow"}
-        
-        k = ""
-        for k in colors.keys():
-            if k in cours["nom"]:
-                key = k 
-                break
-
-        color = colors.get(k) or "yellow"
-
-        # bloc text
-        TOP_TEXT = cours['nom']
-        BOTTOM_TEXT = cours['salle'] or ""
-
-        # create bloc
-        img = Image.new('RGB', (X_LENGHT,Y_LENGHT*size), color)
-        draw = ImageDraw.Draw(img)
-
-        # draw text
-        x_position = X_LENGHT//3
-        y_position = (Y_LENGHT)*size//3
-
-        draw.text((vertical_alignement(TOP_TEXT, fnt_bold),y_position)   , TOP_TEXT   , font=fnt_bold, fill=(0, 0, 0))
-        draw.text((vertical_alignement(BOTTOM_TEXT, fnt),y_position+25), BOTTOM_TEXT, font=fnt, fill=(0, 0, 0))
-        
-        liste_image.append(img)
-
-    # paste courses
-    for i,image in enumerate(liste_cours):
-        first_hour = liste_cours[i]['heures'][0] - 1
-        im.paste(liste_image[i] ,(0,Y_LENGHT*(first_hour-7)))
-
-    # colles et TP
-    jour = monday + timedelta(days=jour_index)
-    events = get_events_of_the_day(jour,groupe)
-
-    for event in events:
-        room = event['room'] if event['room'] else '.'
-        prof_name = event['teatcher']
-        start_hour = event['timedelta']['hours']
-        size = 1
-        event_name = f"{event['subject']}"
-
-        if event['type'] == 'colle':
-            size = 1
-            color = 'gray'
-        elif event['type'] == 'TIPE':
-            size = 2
-            color = 'yellow'
-        elif event['type'] == 'tp':
-            size = 3
-            color = 'yellow'
-
-        # create bloc
-        img = Image.new('RGB', (X_LENGHT,Y_LENGHT*size), color)
-        draw = ImageDraw.Draw(img)
-
-        # draw text
-        TOP_TOP_TEXT = event['type']
-        TOP_TEXT = event_name
-        MIDDLE_TEXT = prof_name
-        BOTTOM_TEXT = room
-        
-        x_position = X_LENGHT //3
-        y_position = (Y_LENGHT)*size//3
-        
-        TEXTS = [ (event['type'],fnt_bold), (event_name,fnt_bold),(prof_name,fnt),(room,fnt)]
-
-        text_heights = [t[1].getsize(t[0])[1] for t in TEXTS]
-
-        # Draw text with vertical and horizontal alignement
-        for i, TEXT in enumerate(TEXTS):
-            draw.text((vertical_alignement(TEXT[0], TEXT[1]),(Y_LENGHT*size - sum(text_heights) )// 2 + 25 * i ), TEXT[0],font=TEXT[1], fill=(0, 0, 0))
-
-        # #paste the image
-        im.paste(img ,(0,Y_LENGHT*(start_hour-8)))
-    
-    #add additional lines
-    for i,cours in enumerate(liste_cours):
-        Ldraw = ImageDraw.Draw(im)
-        
-        first_heure = liste_cours[i]['heures'][0] - 8
-        last_heure = liste_cours[i]['heures'][-1] - 7
-
-        points = [
-            (0,Y_LENGHT * first_heure), 
-            (X_LENGHT,Y_LENGHT *first_heure)
-        ]
-        
-        Ldraw.line(points, fill ="black", width = 1)
-        
-        points = [
-            (0,Y_LENGHT *last_heure), 
-            (X_LENGHT,Y_LENGHT *last_heure)
-        ]
-
-        
-        Ldraw.line(points, fill ="black", width = 1)
-    return im
-
+from class_file import *
+from tools import data_io
+from tools import timetable
 
 class PlanningCommands(commands.Cog):
     def __init__(self, client:RaspailAssistant):
@@ -205,147 +21,69 @@ class PlanningCommands(commands.Cog):
 
     @cog_ext.cog_slash(name="planning",description='T\'envoie ton planning de la semaine ou de la semaine prochaine si on est la weekend :)',guild_ids= [879451596247933039])
     async def send_planning(self,ctx:SlashContext):
-        X_LENGHT = 300
-        Y_LENGHT = 100
-        DX = 50
-        with open('datas/edt.json','r') as item:
-            edt = json.load(item)
-            edt = edt['edt']
-        fnt_high = ImageFont.truetype('datas/Roboto-Bold.ttf', Y_LENGHT//4)
+
         if not await self.client.database.user_in_database(ctx.author.id):
             await ctx.send('Utilise /groupe avant tout ;)')
             return
-        today = date.today()
+        today = datetime.date.today()
         user_grp = (await self.client.database.get_user_info(ctx.author.id))["group"] + 1
 
         if today.weekday() < 5:
-            monday = today - timedelta(days = today.weekday())
+            monday = today - datetime.timedelta(days = today.weekday())
         else:
-            monday = today - timedelta(days = today.weekday()) + timedelta(days = 7)
+            monday = today - datetime.timedelta(days = today.weekday()) + datetime.timedelta(days = 7)
 
-        column = datas["mondays"].index(monday.strftime("%d/%m/%Y")) 
 
-        events = []
-        for line in datas["planning"]:
-            if line["grps"][column] == user_grp :
-                events.append(line)
-        
-        events.sort(key= lambda line : timedelta(**line["timedelta"]))
+        # Start the creation of a timetable
 
-        message = f'La semaine du lundi {monday.strftime("%d/%m/%Y")}:'
-        for event in events:
-            message += f"\n\t - Tu as {event['type']} {('de ' + event['subject'] + ' ') if event['type'] == 'colle' else ''}avec {event['teatcher']} le {DAYS[event['timedelta']['days']]} à {event['timedelta']['hours']}h {' dans la salle ' + event['room'] + '.' if event['room'] else '.'}".format(event = event)
-        
-        #create the Image
-        image = Image.new('RGB', (X_LENGHT * 5 + DX, Y_LENGHT*11), color = 'white')
-        
-        individual_imag = [ get_day_planing(user_grp,get_week_parity(monday),jour,monday)  for jour in edt ]
-        
-        for i,img in enumerate(individual_imag):
-            image.paste(img ,(DX + X_LENGHT*i,0))
-        
-        #heures
-        draw = ImageDraw.Draw(image)
-        for i in range(8,19):
-            x_position = 0
-            y_position = Y_LENGHT * (i - 8)
-            draw.text((x_position,y_position), f'{i}h', font=fnt_high, fill=(0, 0, 0))
-        
-        #lignes verticales
-        for j in range(6):
-            Ldraw = ImageDraw.Draw(image)
-            points = [
-                    (DX + j*X_LENGHT,0 ), 
-                    (DX + j*X_LENGHT,Y_LENGHT *11)
+        timetable_I = timetable.timtable_imager(timetable_dimention_width = 6)
+
+        colums = []
+        for day in range(0, 6):
+            events = data_io.get_events_of_the_day(monday + datetime.timedelta(days=day), user_grp)
+            blocks = []
+            for event in events:
+
+                lines = [
+                    {
+                        "content": event.get("nom", "") or event.get("subject", ""),
+                        "font": "bold",
+                        "align": ""
+                    },
+                    {
+                        "content": event.get("salle","") or event.get("salle","") or "salle inconue",
+                        "font": "regular",
+                        "align": ""
+                    }
                 ]
-            Ldraw.line(points, fill ="black", width = 1)
-        #lignes heures
-        for j in range(11):
-            Ldraw = ImageDraw.Draw(image)
-            points = [
-                    (0,Y_LENGHT *j), 
-                    (DX,Y_LENGHT *j)
-                ]
-            Ldraw.line(points, fill ="black", width = 1)
+
+                if event.get("teatcher", ""):
+                    lines.append({
+                        "content": event.get("teatcher", ""),
+                        "font": "regular",
+                        "align": ""
+                    })
+                
+                height =  event["duration"]["hours"]
+                color =  timetable.get_color(event.get("nom", event.get("subject", "")).split(" - ")[0].lower())
+                blocks.append(timetable_I.generate_block(lines, height, color))
+            
+            colums.append(timetable_I.generate_column(blocks, [event["timedelta"]["hours"] - 8 for event in events]))
+
+
+        # Generate the image
+        image = timetable_I.generate_timetable(colums)
+
         #send the planning
         buffer_output = io.BytesIO()
         image.save(buffer_output, format='PNG')
         buffer_output.seek(0)
         file = discord.File(buffer_output, 'edt.png')
         #send the file
-        await ctx.send(content=message,file=file)
+        await ctx.send(content=f"Voila pour toi l'edt du groupe {user_grp} :) !",file=file)
     
-    @cog_ext.cog_slash(name="edt",description='T\'envoie l\'emploie du temps du jour choisis',guild_ids= [879451596247933039])
-    async def edt_image(self,ctx:SlashContext):
-        def check(context:ComponentContext):
-            return ctx.author == context.author
-        if not await self.client.database.user_in_database(ctx.author.id):
-            await ctx.send('utilise /groupe avant tout ;)')
-            return
-        #get group
-        groupe = (await self.client.database.get_user_info(ctx.author.id))["group"] + 1
-        #-----------------compute week------------------#
-        today = date.today()
-        if today.weekday() < 5:
-            monday = today - timedelta(days = today.weekday())
-        else:
-            monday = today - timedelta(days = today.weekday()) + timedelta(days = 7)
-        #week parity
-        week_parity = get_week_parity(monday)
-        #-----------------------------------------------#
 
-        #-----------------------------#
-        #declare constant
-        X_LENGHT = 300
-        Y_LENGHT = 100
-        DX = 50
-
-        with open('datas/edt.json','r') as item:
-            edt = json.load(item)
-            edt = edt['edt']
-        
-        select = create_select(
-        options=[
-            create_select_option(f"{jour}", value=f"{i}", emoji='📅')
-            for i,jour in enumerate(DAYS)
-        ],
-        placeholder="Utilise ce menu déroulant pour séléctionner le jour",
-        min_values=1, # the minimum number of options a user must select
-        max_values=1  # the maximum number of options a user can select
-        )
-        action_row = create_actionrow(select)
-        embed= discord.Embed(title='Sélectione un jour')
-        embed.set_image(url='https://media.gettyimages.com/photos/happy-student-in-class-picture-id539246041?s=612x612')
-        #send the embed and selection
-        await ctx.send(embed=embed,components=[action_row])
-
-        #wait for selection
-        select_ctx: ComponentContext = await wait_for_component(self.client, components=action_row,check=check)
-        
-        jour_index = int(select_ctx.selected_options[0])
-        jour = edt[jour_index]
-        
-        #create the reponse button
-        buttons = [
-            create_button(
-                style=ButtonStyle.green,
-                label=f"Tu as Sélectioner un jour",
-                disabled=True,
-                emoji='📅'
-            ),
-        ]
-        #respond to the orginal message
-        await select_ctx.edit_origin(embed=embed,components=[create_actionrow(*buttons)])
-        
-        im =  get_day_planing(groupe,get_week_parity(monday),jour,monday)
-        #send the planning
-        buffer_output = io.BytesIO()
-        im.save(buffer_output, format='PNG')
-        buffer_output.seek(0)
-        file = discord.File(buffer_output, 'edt.png')
-        await ctx.send(file=file,content=f'Voici le planning du {DAYS[jour_index]} {jour.strftime("%d/%m/%Y")}')
-
-    @cog_ext.cog_slash(name="papier",description='T\'envoie l\'emploie du temps et coloscope papier.',guild_ids= [879451596247933039])
+    @cog_ext.cog_slash(name="papier",description='T\'envoie l\'emploie du temps et coloscope en version papier.',guild_ids= [879451596247933039])
     async def papier(self,ctx:SlashContext):
         edt = discord.File(open("datas/edt.jpg", "rb"), 'edt.png')
         colloscope = discord.File(open("datas/colloscope.jpg", "rb"), 'colloscope.png')
@@ -357,7 +95,10 @@ def setup(bot:RaspailAssistant):
     
 
 """
-TODO LIST
+TODOLIST:
+
 TODO: /now Renvoie pour chaque grp le cours actuel
-TODO: Presence
+TODO: /today Renvoie le planning du jour
+TODO: /dashboard -> envoie un lien vers une app heroku qui donne l'edt en fontion d'une date choisie + planning de colle maths & physique.
+TODO: Presence <-- 
 """
