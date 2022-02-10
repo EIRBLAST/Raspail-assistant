67a747efe0764b18abe6b10b8f29482f909253cf
JulesMichael <julesmichaelmail@gmail.com>
2022-02-10 23:22:47 +0100

    Ajout de la commande `/oracle`

--- cogs/planning_commands.py
+++ cogs/planning_commands.py
@@ -1,5 +1,5 @@
 import io
-from time import time
+import time
 import datetime
 
 from discord.ext import commands
@@ -89,12 +89,16 @@ class PlanningCommands(commands.Cog):
 
     @cog_ext.cog_slash(name="now",description='T\'envoie le planning de chaque groupe ',guild_ids= [879451596247933039])
     async def send_planning_now(self,ctx:SlashContext):
+        """
+            Envoie le planning de chaque groupe
+        """
         nb_of_groups = 12
         today = datetime.date.today()
 
         if today.weekday() > 4:
             await ctx.send(content="Tu es en weekend, tu ne peux pas faire ça :).")
             return 
+        
 
 
         events_of_grp = lambda i: data_io.get_events_of_the_day(today, i)
@@ -147,12 +151,125 @@ class PlanningCommands(commands.Cog):
         #send the file
         await ctx.send(content=f"Voila pour toi voici le planning d'aujourd'hui ({today.strftime('%d/%m/%Y')}) :) !",file=file)
     
+    
 
     @cog_ext.cog_slash(name="papier",description='T\'envoie l\'emploi du temps et coloscope en version papier.',guild_ids= [879451596247933039])
     async def papier(self,ctx:SlashContext):
+        """
+            T'envoie l'emploi du temps et coloscope en version papier.
+        """
         edt = discord.File(open("datas/edt.jpg", "rb"), 'edt.png')
         colloscope = discord.File(open("datas/colloscope.jpg", "rb"), 'colloscope.png')
         await ctx.send(files=[edt, colloscope],content='Voici pour toi :) !')
+    
+    # Permet de voir le planning d'un groupe jusquà 6 semaines à l'avance
+    @cog_ext.cog_slash(name="oracle",description='T\'envoie le planning d\'un groupe jusqu\'à 6 semaines à l\'avance.',guild_ids= [879451596247933039])
+    async def oracle(self,ctx:SlashContext):
+        """
+            T'envoie le planning d'un groupe jusqu'à 6 semaines à l'avance.
+        """
+
+        print("Bon je commence")
+
+        today = datetime.date.today()
+        next_mondays = [today - datetime.timedelta(days=today.weekday()) + datetime.timedelta(days=i*7) for i in range(1,7)]
+
+        print("Heooo ?")
+        if not await self.client.database.user_in_database(ctx.author.id):
+            await ctx.send('Utilise /groupe avant tout ;)')
+            return
+        user_grp = (await self.client.database.get_user_info(ctx.author.id))["group"] + 1
+        # user_role_mentionner = list(filter(lambda r: r.name == "Groupe "+ str(user_grp), ctx.channel.guild.roles ))[0].mention
+
+        print("Heooo2 ?")
+        # Selection par l'utilisateur de la semaine à afficher
+
+        embed = discord.Embed(title="Selectionne la semaine", description="", color=0x00ff00)
+        embed.set_image(url="https://image.shutterstock.com/image-photo/male-fortune-teller-crystal-ball-600w-2023087094.jpg")
+        selection = create_select(
+            options = [create_select_option("Semaine du lundi " + d.strftime("%d/%m/%Y"), value=d.strftime("%d/%m/%Y")) for d in next_mondays],
+            placeholder = "Utilise ce menu déroulant pour séléctionner les semaines à afficher",
+            min_values= 1,
+            max_values = 6
+        )
         
+        action_row = create_actionrow(selection)
+        await ctx.send(embed=embed, components=[action_row])
+
+        print('hey')
+        select_ctx: ComponentContext = await wait_for_component(self.client, components=action_row,check=lambda c: ctx.author == c.author)
+        
+        slected_weeks = select_ctx.selected_options
+        print("Selected weeks:", slected_weeks)
+
+        #create the reponse button
+        buttons = [
+            create_button(
+                style=ButtonStyle.green,
+                label=f"C'est noté maxi bg!",
+                disabled=True,
+            ),
+        ]
+        #respond to the orginal message
+        await select_ctx.edit_origin(embed=embed,components=[create_actionrow(*buttons)])
+
+        # Send plannings
+
+        for week in slected_weeks:
+            # Start the creation of a timetable
+            monday = datetime.datetime.strptime(week, "%d/%m/%Y")
+            print("monday:",monday)
+
+            timetable_I = timetable.timtable_imager(timetable_dimention_width = 6)
+
+            colums = []
+            for day in range(0, 6):
+                events = data_io.get_events_of_the_day(monday + datetime.timedelta(days=day), user_grp)
+                blocks = []
+                for event in events:
+                    if event.get("type", "") == "colle":
+                        color =  "grey"
+                    else:
+                        color =  timetable.get_color(event.get("nom", event.get("subject", "")).split(" - ")[0].lower())
+
+                    lines = [
+                        {
+                            "content": event.get("subject", "").upper(),
+                            "font": "bold",
+                            "align": ""
+                        },
+                        {
+                            "content": event.get("room","") or "salle inconue",
+                            "font": "regular",
+                            "align": ""
+                        }
+                    ]
+
+                    if event.get("teatcher", ""):
+                        lines.append({
+                            "content": event.get("teatcher", ""),
+                            "font": "regular",
+                            "align": ""
+                        })
+                    
+                    height =  event["duration"]["hours"]
+                    
+                    blocks.append(timetable_I.generate_block(lines, height, color))
+                colums.append(timetable_I.generate_column(blocks, [event["timedelta"]["hours"] - 8 for event in events], header = data_io.DAYS[day] + " \n" + monday.strftime("%d/%m/%Y")))
+
+
+            # Generate the image
+            image = timetable_I.generate_timetable(colums)
+
+            #send the planning
+            buffer_output = io.BytesIO()
+            image.save(buffer_output, format='PNG')
+            buffer_output.seek(0)
+            file = discord.File(buffer_output, 'edt.png')
+            #send the file
+            await ctx.send(content=f"Voila pour toi l'edt de la semaine du {week} :) !",file=file)
+            time.sleep(0.2)
+        
+
 def setup(bot:RaspailAssistant):
     bot.add_cog(PlanningCommands(bot))
